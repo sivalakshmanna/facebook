28fb4774c725c985369fd6a73d7029bc042c87b9
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-21 18:58:17 +0530

    updated script

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -1,6 +1,10 @@
 //Declarative
 pipeline{
     agent any
+    environment {
+        string(name: 'BRANCH_NAME', defaultvalue: 'master', description: 'From which branch artifacts wants to deploy?')
+        string(name: 'BUILD_NUM', defaultvalue: '', description: 'From which build number artifacts wants to deploy?')
+    }
     stages{
         stage("download artifacts"){
           steps{
@@ -10,6 +14,10 @@ pipeline{
         stage("copy artifacts"){
             steps{
                 println "Here i'm copying artifacts from jenkins to tomcat server"
+                sh """aws s3 ls 
+                aws s3 ls s3://sivabandela
+                aws s3 ls s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
+                   """
             }
         }
     }
