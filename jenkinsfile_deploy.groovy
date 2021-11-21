21fa0bd100e86a378186a35ade36d0d9ad01b4e0
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-21 19:12:19 +0530

    updated script

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -9,15 +9,16 @@ pipeline{
         stage("download artifacts"){
           steps{
             println " Here I'm downloading artifacts from s3"
+            sh """
+                   aws s3 ls 
+                   aws s3 ls s3://sivabandela
+                   aws s3 ls s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
+               """
             }
         }
         stage("copy artifacts"){
             steps{
                 println "Here i'm copying artifacts from jenkins to tomcat server"
-                sh """aws s3 ls 
-                aws s3 ls s3://sivabandela
-                aws s3 ls s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
-                   """
             }
         }
     }
