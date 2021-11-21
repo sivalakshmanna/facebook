c6334c3f20ecac6c345b18033cda00190f9ef662
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-21 19:42:39 +0530

    updated script

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -4,6 +4,7 @@ pipeline{
     parameters {
         string(name: 'BRANCH_NAME', defaultvalue: 'master', description: 'From which branch artifacts wants to deploy?')
         string(name: 'BUILD_NUM', defaultvalue: '', description: 'From which build number artifacts wants to deploy?')
+        string(name: 'SERVER_IP', defaultvalue: '', description: 'to which want to deploy?')
     }
     stages{
         stage("download artifacts"){
@@ -13,12 +14,14 @@ pipeline{
                    aws s3 ls 
                    aws s3 ls s3://sivabandela
                    aws s3 ls s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/
+                   aws s3 cp s3://sivabandela/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
                """
             }
         }
         stage("copy artifacts"){
             steps{
                 println "Here i'm copying artifacts from jenkins to tomcat server"
+                sh "ssh -i /tmp/sivalakshmanna07.pem ec2-user@${SERVER_IP} "systemctl status tomcat""
             }
         }
     }
