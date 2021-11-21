2755121c7d74dafc227ad7b3bf45fee55e1ee23c
sivalakshmanna <lakshmansiva07@gmail.com>
2021-11-21 11:36:14 +0530

    updated sorce code

--- jenkisfile_build.groovy
+++ jenkisfile_build.groovy
@@ -6,7 +6,7 @@ pipeline{
             steps{
                 println "clone our code to our repository"
                 sh "ls -l"
-                checkout([$class: 'GitSCM', branches: [[name: '*/branchname']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/sivalakshmanna/boxfuse-sample-java-war-hello.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/branchname']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git']]])
                 sh "ls -lart ./*"
 
             }
